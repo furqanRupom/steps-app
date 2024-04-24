0042e7bb74bfabfefb498bc8bed6f230e1f1d9d8
furqanrupom <furqanrupom978@gmail.com>
2024-04-25 01:32:27 +0600

    feedback/page.tsx : added metadata

--- src/app/feedback/page.tsx
+++ src/app/feedback/page.tsx
@@ -5,7 +5,10 @@ import * as React from 'react';
 interface IFeedbackPageProps {
 }
 
-
+export const metadata = {
+  title: "Steps | feedback",
+  description: "give your valuable feedback"
+}
 
 const FeedbackPage: React.FunctionComponent<IFeedbackPageProps> = (props) => {
   return <section className='min-h-screen w-full h-full bg-gradient-to-t from-black to-gray-900 backdrop-blur-2xl py-20'>
